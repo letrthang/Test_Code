9917a2834c94367b6e18d075ecfd5e4b889054f4
ThangLe-letrt <letrthang@hotmail.com>
2018-07-29 17:10:53 +0800

    de

--- Algorithms/src/NoisyBinarySearch.cpp
+++
@@ -1,6 +0,0 @@
-/**
- * NoisyBinarySearch.cpp
- * Noisy Binary Search
- *  Created on: Jul 13, 2015
- * Author: Thang Le
- */
--- TestC/src/namespace.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using std::cout;
-int main17 () {
-  cout << "Hello world in ANSI-C++\n";
-  getchar();
-  return 0;
-}
